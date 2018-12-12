e6902cacfa264747de6610a06dab62eaf68ddd8e
Shaun Hammill <plloi.pllex@gmail.com>
2018-12-12 08:25:47 -0500

    WIP4

--- commandRouter.go
+++ commandRouter.go
@@ -21,7 +21,7 @@ func NewCommandRouter() *CommandRouter {
 		commands: make(map[string]func(*discordgo.Session, *discordgo.MessageCreate)),
 		helpText: make(map[string]string),
 	}
-	router.commandPrefix = "sal!"
+	router.commandPrefix = "pj!"
 
 	router.setup()
 
@@ -29,8 +29,8 @@ func NewCommandRouter() *CommandRouter {
 }
 
 func (c *CommandRouter) setup() {
-	c.commands["help"] = c.help
-	c.helpText["help"] = "This help text"
+	c.RegisterCommand("help", "This help text", c.help)
+	c.RegisterCommand("echo", "Make the bot say something (Server Admin only)", c.echo)
 }
 
 func (c *CommandRouter) help(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -39,6 +39,12 @@ func (c *CommandRouter) help(s *discordgo.Session, m *discordgo.MessageCreate) {
 		helpMessage += fmt.Sprintf("* %s: %s\n", key, value)
 	}
 	s.ChannelMessageSend(m.ChannelID, helpMessage)
+}
+
+func (c *CommandRouter) echo(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if ok, _ := memberHasPermission(s, m.GuildID, m.Author.ID, discordgo.PermissionAdministrator); ok {
+		s.ChannelMessageSend(m.ChannelID, m.Content)
+	}
 }
 
 // RegisterCommand Adds a command, it's help text, and function to the router
@@ -57,6 +63,12 @@ func (c *CommandRouter) RegisterCommand(command string, help string, f func(*dis
 
 // HandleCommand Takes Discord input and tries to find a relevant command, can be passed to discord-go's AddHandler
 func (c *CommandRouter) HandleCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
 	// Check for bot trigger
 	if len(m.Content) > len(c.commandPrefix) && m.Content[:len(c.commandPrefix)] == c.commandPrefix {
 		// trim prefix
@@ -73,4 +85,38 @@ func (c *CommandRouter) HandleCommand(s *discordgo.Session, m *discordgo.Message
 			s.ChannelMessageSend(m.ChannelID, "Command not recognized")
 		}
 	}
+}
+
+// MemberHasPermission checks if a member has the given permission
+// for example, If you would like to check if user has the administrator
+// permission you would use
+// --- MemberHasPermission(s, guildID, userID, discordgo.PermissionAdministrator)
+// If you want to check for multiple permissions you would use the bitwise OR
+// operator to pack more bits in. (e.g): PermissionAdministrator|PermissionAddReactions
+// =================================================================================
+//     s          :  discordgo session
+//     guildID    :  guildID of the member you wish to check the roles of
+//     userID     :  userID of the member you wish to retrieve
+//     permission :  the permission you wish to check for
+func memberHasPermission(s *discordgo.Session, guildID string, userID string, permission int) (bool, error) {
+	member, err := s.State.Member(guildID, userID)
+	if err != nil {
+		if member, err = s.GuildMember(guildID, userID); err != nil {
+			return false, err
+		}
+	}
+
+	// Iterate through the role IDs stored in member.Roles
+	// to check permissions
+	for _, roleID := range member.Roles {
+		role, err := s.State.Role(guildID, roleID)
+		if err != nil {
+			return false, err
+		}
+		if role.Permissions&permission != 0 {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/plloi/Junior/commands"
 )
 
 // Variables used for command line parameters
@@ -33,6 +34,8 @@ func main() {
 
 	Router = NewCommandRouter()
 
+	Router.RegisterCommand("8ball", "Get A yes/answer from the magic B-ball.", commands.Roll8Ball)
+
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(Router.HandleCommand)
 
@@ -51,24 +54,4 @@ func main() {
 
 	// Cleanly close down the Discord session.
 	dg.Close()
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the autenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
 }
